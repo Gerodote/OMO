3e4c46d1eea0dfde5478a9f1d5f55e6db5b53ef1
Gerodote <lg3dx6fd@gmail.com>
2022-10-31 18:09:36 +0200

    pagerank_first_try

--- omo/pagerank/methods.py
+++ omo/pagerank/methods.py
@@ -1,5 +1,9 @@
-
+from typing import Optional
 import numpy as np
 
-def pagerank_eigenvector(transition_probability_matrix):
-    ...
+def power_method(transition_probability_matrix:np.ndarray, iterations_qty:Optional[int]=100, dampling_factor:float = 0.85):
+    '''
+        https://en.wikipedia.org/wiki/Power_iteration
+    '''
+    eigenvector_approximation = np.random.rand(transition_probability_matrix.shape[1])
+        
--- test_pagerank.py
+++ test_pagerank.py
@@ -1,14 +1,17 @@
 import numpy as np
 import pytest
 
-from omo.pagerank.methods import pagerank_eigenvector
+from omo.pagerank.np_digraph import np_digraph
+
+
+
 
 def test_pagerank():
     probability_matrix = np.array([[0,0.5,0,0.5],
                                    [0,0,1,0],
                                    [0.33,0.33,0,0.33],
                                    [0,0,1,0]])
-    sth = pagerank_eigenvector(probability_matrix)
     result = np.array([0.14,0.21,0.43,0.21])
-    
-    assert (pytest.approx(sth) == result)
+    sth = np_digraph(matrix=probability_matrix)
+    sth_2 = sth.pagerank(dampling_factor = 1)
+    assert (pytest.approx(sth_2) == result)
