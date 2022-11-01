82cf2b6cc2dc7f27736b1f9f81dc580d3f24d09e
Gerodote <lg3dx6fd@gmail.com>
2022-11-01 12:51:17 +0200

    pagerank maybe works

--- omo/pagerank/np_digraph.py
+++ omo/pagerank/np_digraph.py
@@ -1,19 +1,37 @@
+from enum import Enum
 import numpy as np
 import numpy.typing as npt
 from typing import Optional
 
 
-def find_largest_eigenvector_power_method_upto_accuracy(matrix:np.ndarray, eps=0.0001):
+def find_largest_eigenvector_power_method_upto_accuracy(matrix:np.ndarray, eps=0.000001):
     '''https://en.wikipedia.org/wiki/Power_iteration'''
     b_k = np.random.rand(matrix.shape[1])
+    b_k /= np.linalg.norm(b_k,ord=1)
     b_k1 = b_k + np.ones(shape=b_k.shape)
-    while np.norm(ord='nuc',x=b_k1-b_k) > eps:
+    while np.linalg.norm(x=b_k1-b_k) > eps:
+        b_k = b_k1
         b_k1 = matrix @ b_k
-        b_k1_norm = np.linalg.norm(b_k1)
-        b_k = b_k1 / b_k1_norm
+        b_k1 /= np.linalg.norm(b_k1)
+        
 
-    return b_k
+    return b_k1
 
+def is_already_right_stochastic(matrix:np.ndarray, eps=0.02):
+    for i in range(matrix.shape[0]):
+        if np.abs(np.sum(matrix[i]) - 1) >= eps:
+            return False
+    return True
+    
+def is_already_left_stochastic(matrix:np.ndarray, eps=0.02):
+    for i in range(matrix.shape[1]):
+        if np.abs(np.sum(matrix[:,i]) - 1) >= eps:
+            return False
+    return True
+class IsStochastic(Enum):
+    NO=0
+    RIGHT=1
+    LEFT=2
 class np_digraph:
     def _set_value_at_diagonal(self, value):
         for i in range(self._adjacency_matrix.shape[0]):
@@ -112,7 +130,7 @@ class np_digraph:
     def make_adjacency_matrix_right_stochastic(self):
         '''https://en.wikipedia.org/wiki/Stochastic_matrix'''
         new_matrix = np.zeros(shape=self._adjacency_matrix.shape, dtype=np.float64)
-        for j in self._adjacency_matrix:
+        for j in range(self._adjacency_matrix.shape[0]):
             out_edges_qty = self.out_edges_qty(j)
             if out_edges_qty != 0:
                 new_matrix[j] = self._adjacency_matrix[j] / out_edges_qty
@@ -124,17 +142,16 @@ class np_digraph:
     def make_adjacency_matrix_left_stochastic(self):
         '''https://en.wikipedia.org/wiki/Stochastic_matrix'''
         new_matrix = np.zeros(shape=self._adjacency_matrix.shape, dtype=np.float64)
-        for j in self._adjacency_matrix[:,]:
+        for j in range(self._adjacency_matrix.shape[1]):
             out_edges_qty = self.out_edges_qty(j)
             if out_edges_qty != 0:
                 new_matrix[:,j] = self._adjacency_matrix[j] / out_edges_qty
             else:
                 new_matrix[:,j] = np.ones(shape=new_matrix.shape[j],dtype=np.float64) / out_edges_qty
         self._adjacency_matrix = new_matrix
-    
 
     
-    def pagerank(self, personal_vector:Optional[np.ndarray]=None, dampling_factor:float=0.82, eps=0.0001, optimization_eps:bool=True):
+    def pagerank(self, personal_vector:Optional[np.ndarray]=None, dampling_factor:float=0.82, eps=0.0001, optimization_eps:bool=True, is_already_stochastic:IsStochastic=IsStochastic.NO, check_if_is_already_specific_stochastic:bool=True):
         '''
         power iteration method used for finding the largest eigenvector approximation
         
@@ -143,23 +160,34 @@ class np_digraph:
         https://en.wikipedia.org/wiki/PageRank
         '''
         
-        self.make_adjacency_matrix_left_stochastic()
+        if is_already_stochastic == IsStochastic.NO:
+            self.make_adjacency_matrix_left_stochastic()
+        elif is_already_stochastic == IsStochastic.LEFT:
+            if check_if_is_already_specific_stochastic:
+                if not is_already_left_stochastic(self._adjacency_matrix):
+                    raise ValueError("matrix of Graph is not left stochastic")
+        else: # is_already_right_stochastic
+            if check_if_is_already_specific_stochastic:
+                if not is_already_right_stochastic(self._adjacency_matrix):
+                    raise ValueError("matrix of Graph is not right stochastic")
+            self._adjacency_matrix = np.transpose(self._adjacency_matrix)
+        
         vector = None
         if personal_vector is not None:
             if vector.shape[0] != 1 and vector.shape[1] != self._adjacency_matrix.shape[0]:
                 raise TypeError(f"The vector has not appropriate size. It has {vector.shape[0]} on {vector.shape[1]}. It should be 1 on {self._adjacency_matrix.shape[0]}")
-            vector = personal_vector / np.norm(x=personal_vector, ord='nuc')
+            vector = personal_vector / np.linalg.norm(x=personal_vector, ord=1)
         else:
             vector = np.ones(shape=(self._adjacency_matrix.shape[0],1), dtype=np.float64)
-            
         
-        matrix_G =(dampling_factor*self._adjacency_matrix) + ((1 - dampling_factor)*np.eye(self._adjacency_matrix.shape[0]) @ personal_vector)
+        matrix_G =(dampling_factor*self._adjacency_matrix) + ((1 - dampling_factor)*np.eye(self._adjacency_matrix.shape[0]) @ vector)
         
         our_eps = eps
         if optimization_eps == True:
             our_eps = eps * matrix_G.shape[0]
         
-        return find_largest_eigenvector_power_method_upto_accuracy(matrix_G, our_eps)
-        
+        unscaled_result = find_largest_eigenvector_power_method_upto_accuracy(matrix_G, our_eps)
+        return unscaled_result / np.sum(unscaled_result)
+         
         
         
--- test_pagerank.py
+++ test_pagerank.py
@@ -1,11 +1,23 @@
 import numpy as np
 import pytest
 
-from omo.pagerank.np_digraph import np_digraph
+from omo.pagerank.np_digraph import np_digraph, IsStochastic, find_largest_eigenvector_power_method_upto_accuracy
 
 
+@pytest.mark.parametrize('matrix, result', [
+    (np.array([[5,1,0],[0,25,1],[0,0,125]]), np.array([1/12000,1/100,1])), 
+    (np.array([[0.  , 0.  , 0.33, 0.  ],
+       [0.5 , 0.  , 0.33, 0.  ],
+       [0.  , 1.  , 0.  , 1.  ],
+       [0.5 , 0.  , 0.33, 0.  ]]), np.array([0.26,0.39,0.79,0.39]))
+    
+    ])
+def test_power_method_upto_accuracy(matrix, result):
+    sth2 = find_largest_eigenvector_power_method_upto_accuracy(matrix=matrix)
+    assert pytest.approx(result,abs=0.01) == sth2
 
 
+@pytest.mark.parametrize('matrix, result, dampling_factor, is_stochastic, left_or_right_stochastic, check_it')
 def test_pagerank():
     probability_matrix = np.array([[0,0.5,0,0.5],
                                    [0,0,1,0],
@@ -13,5 +25,5 @@ def test_pagerank():
                                    [0,0,1,0]])
     result = np.array([0.14,0.21,0.43,0.21])
     sth = np_digraph(matrix=probability_matrix)
-    sth_2 = sth.pagerank(dampling_factor = 1)
-    assert (pytest.approx(sth_2) == result)
+    sth_2 = sth.pagerank(dampling_factor=1, is_already_stochastic=IsStochastic.RIGHT)
+    assert pytest.approx(result, abs=0.01) == sth_2
