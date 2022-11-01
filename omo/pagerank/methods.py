349acd6f017e6b8526b3a178a550bae22eb459d4
Gerodote <lg3dx6fd@gmail.com>
2022-11-01 15:33:57 +0200

    useless

--- omo/pagerank/methods.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-import numpy as np
-
-def power_method(transition_probability_matrix:np.ndarray, iterations_qty:Optional[int]=100, dampling_factor:float = 0.85):
-    '''
-        https://en.wikipedia.org/wiki/Power_iteration
-    '''
-    eigenvector_approximation = np.random.rand(transition_probability_matrix.shape[1])
-        
